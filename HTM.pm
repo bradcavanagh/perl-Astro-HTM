44c3cb8a5a103ce40fb2c6e8334d854463983c8d
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2005-06-10 00:46:48 +0000

    stub for Astro::HTM
    svn path=/trunk/perlmods/; revision=9109

---
+++ HTM.pm
@@ -0,0 +1,16 @@
+package Astro::HTM;
+
+=head1 NAME
+
+Astro::HTM - Class for handling Heirarchical Triangular Mesh information.
+
+=cut
+
+use 5.006;
+use strict;
+use warnings;
+use warnings::register;
+use Carp;
+
+our $VERSION = '0.01';
+
