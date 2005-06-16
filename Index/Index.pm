9241a12a99f7196c1f610f6e2d61662543ca25aa
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2005-06-16 19:30:17 +0000

    moving from laptop to desktop (this version will not compile)
    svn path=/trunk/perlmods/; revision=9158

--- Index/Index.pm
+++ Index/Index.pm
@@ -19,6 +19,7 @@ use Class::Struct layer => { level => '$',
                              first_vertex => '$',
                            };
 
+use Math::Trig qw/ acos atan /;
 use Math::VectorReal;
 
 use Astro::HTM::QuadNode;
@@ -95,7 +96,7 @@ sub configure {
   my $self = shift;
 
   # Deal with arguments.
-  my %args = @_
+  my %args = @_;
   my ( $maxlevel, $buildlevel );
   if( ! defined( $args{'maxlevel'} ) ) {
     $maxlevel = 20;
@@ -129,7 +130,7 @@ sub configure {
   $self->add_layer( $l0 );
 
   # Set the first six vertices.
-  my $v = [ [ 0, 0, 1 ],
+  my @v = [ [ 0, 0, 1 ],
             [ 1, 0, 0 ],
             [ 0, 1, 0 ],
             [ -1, 0, 0 ],
@@ -282,6 +283,17 @@ sub stored_leaves {
 
 =over 4
 
+=item B<add>
+
+=cut
+
+sub add {
+  my $self = shift;
+  my $vector = shift;
+  my $i = shift;
+
+  $
+
 =item B<add_node>
 
 =cut
@@ -302,6 +314,207 @@ sub add_vertex {
   my $vertex = shift;
 
   push @{$self->{VERTICES}}, $vertex;
+}
+
+=item B<area>
+
+=cut
+
+sub area {
+  my $self = shift;
+  my $n0 = shift;
+  my $n1 = shift;
+  my $n2 = shift;
+
+  my $a = acos( $n0 . $n1 );
+  my $b = acos( $n1 . $n2 );
+  my $c = acos( $n2 . $n0 );
+
+  my $s = ( $a + $b + $c ) / 2;
+  my $area = 4 * atan( sqrt( tan( $s / 2 ) *
+                             tan( ( $s - $a ) / 2 ) *
+                             tan( ( $s - $b ) / 2 ) *
+                             tan( ( $s - $c ) / 2 ) ) );
+
+  return $area;
+}
+
+=item B<area_name>
+
+=cut
+
+sub area_name {
+  my $self = shift;
+  my $htmname = shift;
+
+  my $n = name_to_triangle( $htmname );
+  my @vec;
+  for( my $v = 0; $v < 3; $v++ ) {
+    my $vn = $n->[$v];
+    $vec[$v] = vector( $vn );
+  }
+  return area( $vec[0], $vec[1], $vec[2] );
+}
+
+=item B<get_layer>
+
+=cut
+
+sub get_layer {
+  my $self = shift;
+  my $index = shift;
+
+  my $layers = $self->layers;
+
+  return $layers->[$index];
+}
+
+=item B<get_node>
+
+=cut
+
+sub get_node {
+  my $self = shift;
+  my $index = shift;
+
+  my $nodes = $self->nodes;
+
+  return $nodes->[$index];
+}
+
+=item B<get_vertex>
+
+=cut
+
+sub get_vertex {
+  my $self = shift;
+  my $x = shift;
+
+  my $vertices = $self->vertices;
+
+  return $vertices->[$x];
+}
+
+
+
+=item B<id_by_leaf_number>
+
+=cut
+
+sub id_by_leaf_number {
+  my $self = shift;
+  my $leaf = shift;
+
+  return ( scalar( $self->leaves ) + $leaf );
+}
+
+=item B<leaf_number_by_id>
+
+=cut
+
+sub leaf_number_by_id {
+  my $self = shift;
+  my $id = shift;
+
+  return ( $id - scalar( $self->leaves ) );
+}
+
+=item B<lookup>
+
+=cut
+
+sub lookup {
+  my $self = shift;
+  my $ra = shift;
+  my $dec = shift;
+
+  return lookup( $ra, $dec, $self->maxlevel );
+}
+
+=item B<lookup_id>
+
+=cut
+
+sub lookup_id {
+  my $self = shift;
+  my $ra = shift;
+  my $dec = shift;
+
+  return lookup_id( $ra, $dec, $self->maxlevel );
+}
+
+=item B<make_new_layer>
+
+=cut
+
+sub make_new_layer {
+  my $self = shift;
+  my $oldlayer = shift;
+
+  my $index;
+  my $id;
+  my $newlayer = $oldlayer + 1;
+
+  my $newl = new layer;
+  $self->add_layer( $newlayer, $newl );
+
+  my $oldl = $self->get_layer( $oldlayer );
+  $newl->level( $oldl->level + 1 );
+  $newl->number_of_vertice( $oldl->number_of_vertices + $oldl->number_of_edges );
+  $newl->number_of_nodes( 4 * $oldl->number_of_nodes );
+  $newl->number_of_edges( $newl->number_of_nodes + $newl->number_of_vertices - 1 );
+  $newl->first_index( $self->index );
+  $newl->first_vertex( $oldl->first_vertex + $oldl->number_of_vertices );
+
+  my $ioffset = $oldl->first_index;
+  for( $index = $ioffset; $index < $ioffset + $oldl->number_of_nodes; $index++ ) {
+    my $N = $self->get_node( $index );
+    $id = $N->id << 2;
+    my $childid = $N->child_id;
+    my $v = $N->v;
+    my $w = $N->w;
+    $childid->[0] = $self->new_node( $v->[0], $w->[2], $w->[1], $id++, $index );
+    $childid->[1] = $self->new_node( $v->[1], $w->[0], $w->[2], $id++, $index );
+    $childid->[2] = $self->new_node( $v->[2], $w->[1], $w->[0], $id++, $index );
+    $childid->[3] = $self->new_node( $w->[0], $w->[1], $w->[2], $id++, $index );
+  }
+}
+
+=item B<name_by_leaf_number>
+
+=cut
+
+sub name_by_leaf_number {
+  my $self = shift;
+  my $id = shift;
+
+  return id_to_name( $self->id_by_leaf_number( $id ) );
+}
+
+=item B<new_node>
+
+=cut
+
+sub new_node {
+  my $self = shift;
+  my $v1 = shift;
+  my $v2 = shift;
+  my $v3 = shift;
+  my $id = shift;
+  my $parent = shift;
+
+  my $last = new Astro::HTM::QuadNode;
+  my @v = ( $v1, $v2, $v3 );
+  $last->v( \@v );
+  $last->w( [ 0, 0, 0 ] );
+  $last->child_id( [ 0, 0, 0, 0 ] );
+  $last->id( $id );
+  $last->index( $self->index );
+  $last->parent( $parent );
+
+  $self->add_node( $self->index, $last );
+  $self->index( $self->index + 1 );
+  return( $self->index );
 }
 
 =item B<node_vertex>
@@ -388,6 +601,72 @@ sub show_vertices {
   }
 }
 
+=item B<sort_index>
+
+=cut
+
+sub sort_index {
+  my $self = shift;
+
+  my $oldnodes = $self->nodes;
+  my ( $index, $nonleaf, $leaf );
+
+  for( $index = IOFFSET, $leaf = IOFFSET, $nonleaf = ( scalar( $self->nodes ) - 1 );
+       $index < scalar( $self->nodes );
+       $index++ ) {
+    my $childid = $oldnodes->[$index]->childid;
+    if( $childid->[0] == 0 ) {
+      $self->set_nodes( $leaf, $oldnodes->[$index] );
+      for( my $i = 0; $i < 4; $i++ ) {
+        my $leafn = $self->get_node( $leaf );
+        my $parent = $self->get_node( $leafn->parent );
+        my $parent_childid = $parent->child_id;
+        if( $parent_childid->[$i] == $index ) {
+          $parent_childid->[$i] = $leaf;
+          last;
+        }
+      }
+      $leaf++;
+    } else {
+      my $N = $oldnodes->[$index];
+      my $N_childid = $N->child_id;
+      $self->set_node( $nonleaf, $N );
+      $oldnodes->[$N_childid->[0]]->parent( $nonleaf );
+      $oldnodes->[$N_childid->[1]]->parent( $nonleaf );
+      $oldnodes->[$N_childid->[2]]->parent( $nonleaf );
+      $oldnodes->[$N_childid->[3]]->parent( $nonleaf );
+
+      for( my $i = 0; $i < 4; $i++ ) {
+        my $pn1 = $self->get_nodes( $N->parent );
+        my $pn1_childid = $pn1->child_id;
+        if( $pn1_childid->[$i] == $index ) {
+          $pn1->childid->[$i] = $nonleaf;
+          last;
+        }
+      }
+      $nonleaf--;
+    }
+  }
+  $self->last_saved_leaf_index( $leaf );
+}
+
+=item B<V>
+
+=cut
+
+sub V {
+  my $self = shift;
+  my $index = shift;
+  my $x = shift;
+
+  my $node = $self->get_node( $index );
+  my $node_v = $node->v;
+  my $node_v_x = $node_v->[$x];
+
+  my $vertex = $self->get_vertex( $node_v_x );
+  return $vertex;
+}
+
 =item B<v_max>
 
 =cut
@@ -397,7 +676,7 @@ sub v_max {
 
   my $nv = 6;
   my $ne = 12;
-  my $nf = 8
+  my $nf = 8;
   my $i = $self->buildlevel;
   $self->number_of_nodes( $nf );
 
